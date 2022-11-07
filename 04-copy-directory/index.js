9bd9e9cb51c57c50d322b79cc03db621d8dafc5d
likurg42 <likurg42@gmail.com>
2022-11-07 22:01:42 +0300

    feat: implement copying a directory with files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 
+(async () => {
+  const FILES_PATH = path.join(__dirname, 'files');
+  const FILES_COPY_PATH = path.join(__dirname, 'files-copy');
+
+  fs.access(FILES_COPY_PATH, async (err) => {
+    if (err) {
+      console.log('not exist');
+      // console.error(err);
+      await fs.promises.mkdir(FILES_COPY_PATH);
+    }
+  });
+
+  const files = await fs.promises.readdir(FILES_PATH);
+  const copyFiles = await fs.promises.readdir(FILES_COPY_PATH);
+
+  copyFiles.forEach(async (copyFile) => {
+    if (!files.includes(copyFile)) {
+      await fs.promises.unlink(path.join(FILES_COPY_PATH, copyFile));
+    }
+  });
+
+  files.forEach((file) => {
+    fs
+      .createReadStream(path.join(__dirname, 'files', file))
+      .pipe(fs.createWriteStream(path.join(__dirname, 'files-copy', file)));
+  });
+
+})();
